1997851b6895badc9689b3583d91f181b53bc70f
Alexander Popelnukhin <ave.lucky@gmail.com>
2011-09-01 23:59:15 +0700

    Create models and add fixtures for it.

--- students/models.py
+++ students/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=100)
+    patronymic = models.CharField(max_length=50)
+    birthday = models.DateField()
+    student_ID = models.CharField(max_length=10)
+    student_group = models.ForeignKey('students.Group', blank=True, null=True)
+
+    def __unicode__(self):
+        return "%s - %s %s. %s." % (self.student_group.title, self.surname, self.name[0], self.patronymic[0])
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    head = models.ForeignKey('students.Student', blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
